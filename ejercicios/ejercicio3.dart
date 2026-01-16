58c88bf1a1281dc56792f2df07b7c0aa0f635236
maketas1 <angelgallardoturpin@gmail.com>
2026-01-16 13:28:11 +0100

    arbolito de los huevos

---
+++ ejercicios/ejercicio3.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+
+void pintarLinea(int mitad, int primo) {
+  String hojas = "*";
+  String espacios = " ";
+  String cadena = "";
+  for(int i = mitad; i > 0; i--) {
+    cadena = "$cadena$espacios";
+  }
+  for(int i = 0; i < primo; i++) {
+    cadena = "$cadena$hojas";
+  }
+  print(cadena);
+}
+
+void arbolito() {
+  print("Indique la altura del arbol");
+  String? alturaStr = stdin.readLineSync();
+  int? altura = int.tryParse(alturaStr ?? '0');
+
+  if (altura == null || altura <= 0) {
+    print('❌ Cantidad inválida');
+    return;
+  }
+  List<int> primos = [1];
+  int contador = 1;
+  while(contador < altura) {
+    primos.add(primos[contador-1] + 2);
+    contador++;
+  }
+
+  double mitadDoecimal = primos[primos.length-1]/2;
+  int mitadEntera = mitadDoecimal.toInt();
+  int reducida = mitadEntera;
+  String maceta = "";
+  for(int i = mitadEntera -1; i > 0; i--) {
+    maceta = "$maceta ";
+  }
+  maceta = "$maceta|||";
+  for(int i = 0; i < altura; i++){
+    pintarLinea(reducida, primos[i]);
+    reducida--;
+  }
+  print(maceta);
+}
+
+void main() {
+  arbolito();
+}
