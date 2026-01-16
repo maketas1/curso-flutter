63c8a7952bd0b2b85000e677af8aaf2239c7bcea
maketas1 <angelgallardoturpin@gmail.com>
2026-01-16 13:45:18 +0100

    flecha

--- ejercicios/ejercicio3.dart
+++ ejercicios/ejercicio3.dart
@@ -44,6 +44,37 @@ void arbolito() {
   print(maceta);
 }
 
+void flechaIxquierda() {
+  print("Indique la longitud de la flecha");
+  String? alturaStr = stdin.readLineSync();
+  int? altura = int.tryParse(alturaStr ?? '0');
+
+  if (altura == null || altura <= 0) {
+    print('❌ Cantidad inválida');
+    return;
+  }
+  List<int> cantidad = [1];
+  int contador = 1;
+  while(contador < altura) {
+    cantidad.add(cantidad[contador-1] + 1);
+    contador++;
+  }
+
+  double mitadDoecimal = cantidad[cantidad.length-1]/2;
+  int mitadEntera = mitadDoecimal.toInt();
+  int reducida = mitadEntera;
+  String maceta = "";
+  for(int i = mitadEntera -1; i > 0; i--) {
+    maceta = "$maceta ";
+  }
+  maceta = "$maceta|||";
+  for(int i = 0; i < altura; i++){
+    pintarLinea(reducida+1, cantidad[i]);
+    reducida--;
+  }
+  print(maceta);
+}
+
 void main() {
-  arbolito();
+  flechaIxquierda();
 }
